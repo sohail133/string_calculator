09a490d5a562c86da27a85e8f69c8f70475bdc19
Sohail Karim <sohailkarim66@gmail.com>
2024-09-01 21:15:55 +0500

    Handle new lines as delimiters

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -1,7 +1,7 @@
 class StringCalculator < ApplicationRecord
   def self.add(input)
     return 0 if input.empty?
-    numbers = input.split(",").map(&:to_i)
+    numbers = input.split(/,|\n/).map(&:to_i)
     return numbers.sum
   end
 end
--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -17,5 +17,9 @@ RSpec.describe StringCalculator, type: :model do
     it 'returns the sum of any amount of numbers separated by commas' do
       expect(StringCalculator.add("1,2,3,4,5")).to eq(15)
     end
+
+    it 'handles new lines as delimiters along with commas' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
   end
 end
