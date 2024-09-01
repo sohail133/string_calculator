cbdda016c2243f77e6bf7d8dd3c27921dba291d2
Sohail Karim <sohailkarim66@gmail.com>
2024-09-01 21:13:12 +0500

    Return the number itself for single number input

--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe StringCalculator, type: :model do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+    it 'returns the number itself for a single number string' do
+      expect(StringCalculator.add("1")).to eq(1)
+    end
   end
 end
